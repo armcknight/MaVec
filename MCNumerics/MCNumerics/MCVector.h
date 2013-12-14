68f19fbecc0c5904a965673b510054f342926005
Andrew McKnight <amcknight2718@gmail.com>
2013-12-14 16:33:03 -0500

    added MCVector class

---
+++ MCNumerics/MCNumerics/MCVector.h
@@ -0,0 +1,45 @@
+//
+//  MCVector.h
+//  MCNumerics
+//
+//  Created by andrew mcknight on 12/8/13.
+//  Copyright (c) 2013 andrew mcknight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCVector : NSObject
+
+#pragma mark - Constructors
+
+- (id)initWithValues:(NSArray *)values;
++ (MCVector *)vectorWithValues:(NSArray *)values;
+
+#pragma mark - NSObject overrides
+
+- (BOOL)isEqual:(MCVector *)otherVector;
+- (NSUInteger)hash;
+
+#pragma mark - Inspection
+
+- (NSUInteger)length;
+- (double)valueAtIndex:(NSUInteger)index;
+- (double)maximumValue;
+- (double)minimumValue;
+- (NSUInteger)indexOfMaximumValue;
+- (NSUInteger)indexOfMinimumValue;
+
+#pragma mark - Instance Operations
+
+- (MCVector *)vectorByMultiplyingByScalar:(double)scalar;
+
+#pragma mark - Class Operations
+
++ (MCVector *)sumOfVectorA:(MCVector *)a andVectorB:(MCVector *)b;
++ (MCVector *)differenceOfVectorA:(MCVector *)a andVectorB:(MCVector *)b;
++ (MCVector *)productOfVectorA:(MCVector *)a andVectorB:(MCVector *)b;
++ (MCVector *)quotientOfVectorA:(MCVector *)a andVectorB:(MCVector *)b;
++ (double)dotProductOfVectorA:(MCVector *)a andVectorB:(MCVector *)b;
++ (double)crossProductOfVectorA:(MCVector *)a andVectorB:(MCVector *)b;
+
+@end
---
+++ MCNumerics/MCNumerics/MCVector.m
@@ -0,0 +1,128 @@
+//
+//  MCVector.m
+//  MCNumerics
+//
+//  Created by andrew mcknight on 12/8/13.
+//  Copyright (c) 2013 andrew mcknight. All rights reserved.
+//
+
+#import "MCVector.h"
+#import <Accelerate/Accelerate.h>
+
+@interface MCVector()
+
+@property (strong, nonatomic) NSArray *values;
+@property (assign, nonatomic) double *valuesCArray;
+@property (assign, nonatomic) NSUInteger length;
+
+@end
+
+@implementation MCVector
+
+#pragma mark - Constructors
+
+- (id)initWithValues:(NSArray *)values
+{
+    self = [super init];
+    if (self) {
+        self.values = values;
+        self.valuesCArray = malloc(values.count * sizeof(double));
+        self.length = values.count;
+        [values enumerateObjectsUsingBlock:^(NSNumber *value, NSUInteger idx, BOOL *stop) {
+            self.valuesCArray[idx] = value.doubleValue;
+        }];
+    }
+    return self;
+}
+
++ (MCVector *)vectorWithValues:(NSArray *)values
+{
+    return [[MCVector alloc] initWithValues:values];
+}
+
+#pragma mark - NSObject overrides
+
+- (BOOL)isEqual:(MCVector *)otherVector
+{
+    BOOL __block equal = self == otherVector.self;
+    
+    if (equal) {
+        return equal;
+    } else {
+        equal = YES;
+        for (int i = 0; i < self.length; i++) {
+            if (self.valuesCArray[i] != [otherVector valueAtIndex:i]) {
+                equal = NO;
+                break;
+            }
+        }
+        return equal;
+    }
+}
+
+- (NSUInteger)hash
+{
+    return self.values.hash;
+}
+
+#pragma mark - Inspection
+
+- (NSUInteger)length
+{
+    return self.values.count;
+}
+
+- (double)valueAtIndex:(NSUInteger)index
+{
+    return self.valuesCArray[index];
+}
+
+- (double)maximumValue
+{
+    double max = DBL_MIN;
+    for (int i = 0; i < self.length; i++) {
+        if (self.valuesCArray[i] > max) {
+            max = self.valuesCArray[i];
+        }
+    }
+    return max;
+}
+
+- (double)minimumValue
+{
+    double min = DBL_MAX;
+    for (int i = 0; i < self.length; i++) {
+        if (self.valuesCArray[i] < min) {
+            min = self.valuesCArray[i];
+        }
+    }
+    return min;
+}
+
+- (NSUInteger)indexOfMaximumValue
+{
+    double max = DBL_MIN;
+    NSUInteger idx = -1;
+    for (int i = 0; i < self.length; i++) {
+        if (self.valuesCArray[i] > max) {
+            idx = i;
+        }
+    }
+    return max;
+}
+
+- (NSUInteger)indexOfMinimumValue
+{
+    double min = DBL_MAX;
+    NSUInteger idx = -1;
+    for (int i = 0; i < self.length; i++) {
+        if (self.valuesCArray[i] < min) {
+            idx = i;
+        }
+    }
+    return min;
+}
+
+#pragma mark - Operations
+
+@end
