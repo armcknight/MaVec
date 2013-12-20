eaca755f4d8c4787138bf89c0d58edee5e1659ba
Andrew McKnight <amcknight2718@gmail.com>
2013-12-20 00:05:25 -0500

    built approximation demo

--- MCNumerics/MCNumerics/LinearApproximationViewController.m
+++ MCNumerics/MCNumerics/LinearApproximationViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "LinearApproximationViewController.h"
+#import "ApproximationView.h"
 
 @interface LinearApproximationViewController ()
+
+@property (strong, nonatomic) IBOutlet ApproximationView *approximationView;
+@property (strong, nonatomic) IBOutlet UISlider *orderSlider;
+@property (strong, nonatomic) IBOutlet UILabel *orderLabel;
 
 @end
 
@@ -17,6 +22,22 @@
 - (id)init
 {
     return [super initWithNibName:@"LinearApproximationView" bundle:nil];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.navigationController.navigationBar.translucent = NO;
+    self.approximationView.order = 3;
+    self.orderLabel.text = @"2";
+    [self.orderSlider setValue:0.3 animated:YES];
+}
+
+- (IBAction)changedOrder:(id)sender {
+    int order = self.orderSlider.value * 10;
+    self.approximationView.order = order;
+    self.orderLabel.text = [NSString stringWithFormat:@"%u", order-1];
 }
 
 @end
