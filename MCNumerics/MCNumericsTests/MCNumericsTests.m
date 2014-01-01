d7dc94d64ad5eab851d97b6c7f62fe32b0b006a1
Andrew McKnight <amcknight2718@gmail.com>
2013-12-31 22:52:00 -0500

    uncommented accelerate import

--- MCNumerics/MCNumericsTests/MCNumericsTests.m
+++ MCNumerics/MCNumericsTests/MCNumericsTests.m
@@ -7,7 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
-//#import <Accelerate/Accelerate.h>
+#import <Accelerate/Accelerate.h>
 #import "MCMatrix.h"
 #import "MCVector.h"
 #import "MCSingularValueDecomposition.h"
