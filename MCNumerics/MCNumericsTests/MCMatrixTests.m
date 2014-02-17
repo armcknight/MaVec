de72c119312830e51298d9b3a5280d3fbd71a86d
Andrew McKnight <amcknight2718@gmail.com>
2014-02-17 17:40:56 -0500

    added transposing a matrix with row major value storage to unit test

--- MCNumerics/MCNumericsTests/MCMatrixTests.m
+++ MCNumerics/MCNumericsTests/MCMatrixTests.m
@@ -687,6 +687,36 @@
             XCTAssertEqual([a valueAtRow:i column:j], [t valueAtRow:i column:j], @"Value at row %u and column %u incorrect", i, j);
         }
     }
+    
+    aVals= malloc(9 * sizeof(double));
+    aVals[0] = 1.0;
+    aVals[1] = 2.0;
+    aVals[2] = 3.0;
+    aVals[3] = 4.0;
+    aVals[4] = 5.0;
+    aVals[5] = 6.0;
+    aVals[6] = 7.0;
+    aVals[7] = 8.0;
+    aVals[8] = 9.0;
+    a = [MCMatrix matrixWithValues:aVals rows:3 columns:3 valueStorageFormat:MCMatrixValueStorageFormatRowMajor];
+    
+    tVals= malloc(9 * sizeof(double));
+    tVals[0] = 1.0;
+    tVals[1] = 4.0;
+    tVals[2] = 7.0;
+    tVals[3] = 2.0;
+    tVals[4] = 5.0;
+    tVals[5] = 8.0;
+    tVals[6] = 3.0;
+    tVals[7] = 6.0;
+    tVals[8] = 9.0;
+    t = [MCMatrix matrixWithValues:tVals rows:3 columns:3 valueStorageFormat:MCMatrixValueStorageFormatRowMajor].transpose;
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            XCTAssertEqual([a valueAtRow:i column:j], [t valueAtRow:i column:j], @"Value at row %u and column %u incorrect", i, j);
+        }
+    }
 }
 
 - (void)testMatrixCreationFromRowVectors
