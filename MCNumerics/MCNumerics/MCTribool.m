e00f3797409893d23f5009326c64275e2c18de8f
Andrew McKnight <amcknight2718@gmail.com>
2014-03-14 22:39:12 -0400

    implementing triboolean operator methods

--- MCNumerics/MCNumerics/MCTribool.m
+++ MCNumerics/MCNumerics/MCTribool.m
@@ -8,7 +8,18 @@
 
 #import "MCTribool.h"
 
+typedef enum : NSUInteger {
+    MCTriboolBinaryOperationAnd,
+    MCTriboolBinaryOperationOr,
+    MCTriboolBinaryOperationKleeneImplication,
+    MCTriboolBinaryOperationLukasiewiczImplication
+} MCTriboolBinaryOperation;
+
 @interface MCTribool ()
+
+- (MCTribool *)performBinaryOperation:(MCTriboolBinaryOperation)operation
+                             triboolA:(MCTribool *)triboolA
+                             triboolB:(MCTribool *)triboolB;
 
 @end
 
@@ -36,39 +47,104 @@
 
 - (BOOL)isYes
 {
-    return _triboolValue == MCTriboolIndeterminate ? NO : _triboolValue == MCTriboolYes ? YES : NO;
+    return (_triboolValue == MCTriboolYes);
 }
 
 #pragma mark - Logical operations
 
 - (MCTribool *)andTribool:(MCTribool *)tribool
 {
-    // TODO: implement based on triboolean logic
-    return nil;
+    return [self performBinaryOperation:MCTriboolBinaryOperationAnd triboolA:self triboolB:tribool];
 }
 
 - (MCTribool *)orTribool:(MCTribool *)tribool
 {
-    // TODO: implement based on triboolean logic
-    return nil;
+    return [self performBinaryOperation:MCTriboolBinaryOperationOr triboolA:self triboolB:tribool];
 }
 
 - (MCTribool *)negate
 {
-    // TODO: implement
-    return nil;
+    if (self.triboolValue == MCTriboolIndeterminate) {
+        return [MCTribool triboolWithValue:MCTriboolIndeterminate];
+    } else {
+        return [MCTribool triboolWithValue:![self isYes]];
+    }
 }
 
 - (MCTribool *)kleeneImplication:(MCTribool *)tribool
 {
-    // TODO: implement
-    return nil;
+    return [self performBinaryOperation:MCTriboolBinaryOperationKleeneImplication triboolA:self triboolB:tribool];
 }
 
 - (MCTribool *)lukasiewiczImplication:(MCTribool *)tribool
 {
-    // TODO: implement
-    return nil;
+    return [self performBinaryOperation:MCTriboolBinaryOperationLukasiewiczImplication triboolA:self triboolB:tribool];
+}
+
+- (MCTribool *)performBinaryOperation:(MCTriboolBinaryOperation)operation
+                             triboolA:(MCTribool *)triboolA
+                             triboolB:(MCTribool *)triboolB
+{
+    MCTribool *result;
+    
+    if (triboolA.triboolValue == MCTriboolIndeterminate) {
+        if (triboolB.triboolValue == MCTriboolIndeterminate) {
+            if (operation == MCTriboolBinaryOperationLukasiewiczImplication) {
+                result = [MCTribool triboolWithValue:MCTriboolYes];
+            } else {
+                result = [MCTribool triboolWithValue:MCTriboolIndeterminate];
+            }
+        } else {
+            switch (operation) {
+                default: case MCTriboolBinaryOperationAnd:
+                    result = [MCTribool triboolWithValue:[triboolB isYes] ? MCTriboolIndeterminate : MCTriboolNo];
+                    break;
+                case MCTriboolBinaryOperationOr:
+                    result = [MCTribool triboolWithValue:[triboolB isYes] ? MCTriboolYes : MCTriboolIndeterminate];
+                    break;
+                case MCTriboolBinaryOperationKleeneImplication:
+                case MCTriboolBinaryOperationLukasiewiczImplication:
+                    result = [MCTribool triboolWithValue:[triboolB isYes] ? MCTriboolYes : MCTriboolIndeterminate];
+                    break;
+            }
+        }
+    } else if (triboolB.triboolValue == MCTriboolIndeterminate) {
+        if (triboolA.triboolValue == MCTriboolIndeterminate) {
+            if (operation == MCTriboolBinaryOperationLukasiewiczImplication) {
+                result = [MCTribool triboolWithValue:MCTriboolYes];
+            } else {
+                result = [MCTribool triboolWithValue:MCTriboolIndeterminate];
+            }
+        } else {
+            switch (operation) {
+                default: case MCTriboolBinaryOperationAnd:
+                    result = [MCTribool triboolWithValue:[triboolA isYes] ? MCTriboolIndeterminate : MCTriboolNo];
+                    break;
+                case MCTriboolBinaryOperationOr:
+                    result = [MCTribool triboolWithValue:[triboolA isYes] ? MCTriboolYes : MCTriboolIndeterminate];
+                    break;
+                case MCTriboolBinaryOperationKleeneImplication:
+                case MCTriboolBinaryOperationLukasiewiczImplication:
+                    result = [MCTribool triboolWithValue:[triboolA isYes] ? MCTriboolIndeterminate : MCTriboolYes];
+                    break;
+            }
+        }
+    } else {
+        switch (operation) {
+            default: case MCTriboolBinaryOperationAnd:
+                result = [MCTribool triboolWithValue:([triboolA isYes] && [triboolB isYes]) ? MCTriboolYes : MCTriboolNo];
+                break;
+            case MCTriboolBinaryOperationOr:
+                result = [MCTribool triboolWithValue:([triboolA isYes] || [triboolB isYes]) ? MCTriboolYes : MCTriboolNo];
+                break;
+            case MCTriboolBinaryOperationKleeneImplication:
+            case MCTriboolBinaryOperationLukasiewiczImplication:
+                result = [MCTribool triboolWithValue:(![triboolA isYes] || [triboolB isYes]) ? MCTriboolYes : MCTriboolNo];
+                break;
+        }
+    }
+    
+    return result;
 }
 
 #pragma mark - NSCopying
