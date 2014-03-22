2bbab709db17983d33882ea72628454aff70b266
Andrew McKnight <amcknight2718@gmail.com>
2014-03-21 21:44:54 -0400

    documentation

--- MCNumerics/MCNumerics/MCVector.h
+++ MCNumerics/MCNumerics/MCVector.h
@@ -168,14 +168,78 @@ MCVectorFormat;
 
 #pragma mark - Class Operations
 
+/**
+ @brief Multiply a vector by a scalar. Effectively multiplies each element in the vector by the scalar and stores the products in a new vector.
+ @param vector Vector to multiply by the scalar.
+ @param scalar Value to multiply each element of the vector by.
+ @return New instance of MCVector holding the result of the scalar multiplication with the same vector format as the provided vector.
+ */
 + (MCVector *)productOfVector:(MCVector *)vector scalar:(double)scalar;
+
+/**
+ @brief Add two vectors together. Effectively adds elements in identical positions and stores the sums in a new vector.
+ @param vectorA The first vector to be added.
+ @param vectorB The second vector to be added.
+ @return New instance of MCVector holding the result of the addition with the same vector format as the provided vectors, or MCVectorFormatRow if they are different.
+ */
 + (MCVector *)sumOfVectorA:(MCVector *)vectorA vectorB:(MCVector *)vectorB;
+
+/**
+ @brief Subtract the subtrahend vector from the minuend vector. Effectively subtracts elements in identical positions and stores the differences in a new vector.
+ @param vectorMinuend The vector from which to subtract the subtrahend.
+ @param vectorSubtrahend The vector to subtract from the minuend.
+ @return New instance of MCVector holding the result of the subtraction with the same vector format as the provided vectors, or MCVectorFormatRow if they are different.
+ */
 + (MCVector *)differenceOfVectorMinuend:(MCVector *)vectorMinuend vectorSubtrahend:(MCVector *)vectorSubtrahend;
+
+/**
+ @brief Multiply two vectors. Effectively multiplies elements in identical positions and stores the products in a new vector.
+ @param vectorA The first vector to be multiplied.
+ @param vectorB The first vector to be multiplied.
+ @return New instance of MCVector holding the result of the multiplication with the same vector format as the provided vectors, or MCVectorFormatRow if they are different.
+ */
 + (MCVector *)productOfVectorA:(MCVector *)vectorA vectorB:(MCVector *)vectorB;
+
+/**
+ @brief Divide the dividend vector by the divisor vector. Effectively divides elements in identical positions and stores the quotients in a new vector.
+ @param vectorDividend The vector to be divided by the divisor.
+ @param vectorDivisor The vector to divide into the dividend.
+ @return New instance of MCVector holding the result of the division with the same vector format as the provided vectors, or MCVectorFormatRow if they are different.
+ */
 + (MCVector *)quotientOfVectorDividend:(MCVector *)vectorDividend vectorDivisor:(MCVector *)vectorDivisor;
+
+/**
+ @brief Compute the dot product of two vectors. Effectively sums the products of elements in identical positions in the vectors.
+ @param vectorA The first vector of the dot product.
+ @param vectorB The second vector of the dot product.
+ @return A scalar value representing the dot product of the vectors.
+ */
 + (double)dotProductOfVectorA:(MCVector *)vectorA vectorB:(MCVector *)vectorB;
+
+/**
+ @brief Compute the cross product of the vectors, which is orthogonal to both input vectors. Currently only computes the cross product of two 3-dimensional vectors.
+ @param vectorA The first vector of the cross product.
+ @param vectorB The second vector of the cross product.
+ @return A new instance representing the cross product of the two vectors in column format.
+ */
 + (MCVector *)crossProductOfVectorA:(MCVector *)vectorA vectorB:(MCVector *)vectorB;
+
+/**
+ @brief Computes the dot product of the last vector and the cross product of the first two vectors, representing the signed volumn of the parallelepiped defined by the three vectors. ( (A x B) • C )
+ @param vectorA The vector on the left side of the cross product.
+ @param vectorB The vector on the left side of the cross product.
+ @param vectorC The vector on the right side of the dot product.
+ @return A scalar value containing the value of the dot product between the third vector and the cross product of the first two vectors.
+ */
 + (double)scalarTripleProductWithVectorA:(MCVector *)vectorA vectorB:(MCVector *)vectorB vectorC:(MCVector *)vectorC;
+
+/**
+ @brief Computes the cross product of the first vector and the cross product of the remaining two vectors. ( A x (B x C) )
+ @param vectorA The vector on the left side of the second cross product.
+ @param vectorB The vector on the left side of the first cross product.
+ @param vectorC The vector on the right side of the first cross product.
+ @return A new instance of MCVector holding the result of the nested cross products.
+ */
 + (MCVector *)vectorTripleProductWithVectorA:(MCVector *)vectorA vectorB:(MCVector *)vectorB vectorC:(MCVector *)vectorC;
 
 @end
