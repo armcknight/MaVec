b6c80040da48d60f1c695113c6dfcb401c610095
Andrew McKnight <amcknight2718@gmail.com>
2014-05-15 21:03:17 -0400

    restructured project, files, demo and tests

---
+++ MaVec-Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MaVec-Demo
+//
+//  Created by Andrew McKnight on 5/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MaVec-Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MaVec-Demo
+//
+//  Created by Andrew McKnight on 5/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
