ee01ec4d34c06807d50cb7c4a63fead01b266474
Andrew McKnight <amcknight2718@gmail.com>
2014-10-26 22:07:36 -0400

    created new header file containing typedefs

---
+++ MaVec/Utilities/MAVTypedefs.h
@@ -0,0 +1,11 @@
+//
+//  MAVTypedefs.h
+//  MaVec
+//
+//  Created by Andrew McKnight on 10/25/14.
+//  Copyright (c) 2014 AMProductions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef __CLPK_integer MAVIndex;
